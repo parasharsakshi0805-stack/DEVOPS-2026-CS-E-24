d1645975ba3c452df26d0b17be3f3315fa0b270f
Sakshi <parasharsakshi0805@gmail.com>
2026-08-24 23:30:19 +0530

    Auth Auth Controller & Register User Implementation

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+const userModel = require("../models/user.model")
+
+
+/**
+ * @name registerUserController
+ * @description register a new user,expects Username,email and password in the request lobby
+ * @access Public
+ */
+async function registerUserController(req,res){}
+
+module.exports = {
+    registerUserController
+}
