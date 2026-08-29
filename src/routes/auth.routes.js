22f3313dbb87ac0b9877fc7c6732d7f39100a3ef
Sakshi <parasharsakshi0805@gmail.com>
2026-08-29 18:53:49 +0530

    Fix middleware import path and JWT payload mismatch in auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const {Router} = require('express')
 const authController = require("../controllers/auth.controller")
 const authRouter = Router()
+const authMiddleware = require("../middlewares/auth.middleware")
 
 /**
  * @route POST /api/auth/register
@@ -19,5 +20,20 @@ authRouter.post("/register",authController.registerUserController)
 authRouter.post("/login",authController.loginUserController)
 
 
+/**
+ * @route GET /api/auth/logout
+ * @description clear token from user cookie and add the token in blacklist
+ * @access public
+ */
+
+authRouter.get("/logout",authController.logoutUserController)
+
+
+/**
+ * @route GET /api/auth/get-me
+ * @description get the current logged in user details
+ * @access private
+ */
+authRouter.get("/get-me", authMiddleware.authUser,authController.getMeController)
 
 module.exports = authRouter
