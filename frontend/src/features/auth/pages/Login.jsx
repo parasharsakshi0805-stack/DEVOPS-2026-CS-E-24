f5bf0fbbf4d2f27f5465ed24918f1f73e4a99a7b
Riddhi Inani <riddhiinani1012@gmail.com>
2026-08-30 11:12:03 +0530

    Add frontend authentication with Axios

---
+++ frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,37 @@
+import react from 'react'
+import { useNavigate,Link } from 'react-router'
+import "../auth.form.scss"
+
+const Login = () => {
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+
+    return (
+        <main>
+            <div className='form-container'>
+                <hi>Login</hi>
+
+                <form onSubmit={handleSubmit}>
+
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type='email' id='email' name='email' placeholder='Enter email address'/>
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type='password' id='password' name='password' placeholder='Enter Password'/>
+                    </div>
+
+                    <button className='button primary-button'>Login</button>
+                </form>
+                <p>Don't have an account? <Link to={"/register"} >Register</Link> </p>
+            </div>
+        </main>
+    )
+}
+
+export default Login
